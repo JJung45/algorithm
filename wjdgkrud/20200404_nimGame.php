ae1a281f5bd6058361601790f75844d007cfbb20
Chung <753cindy@naver.com>
2020-04-04 23:24:25 +0900

    wjdgkrud / nimGame

---
+++ wjdgkrud/20200404_nimGame.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm,
+ * User: 63328
+ * Date: 2020-04-04
+ * Time: 오후 11:23
+ */
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function canWinNim($n) { // n = 4
+        return $n%4; // false
+    }
+}
